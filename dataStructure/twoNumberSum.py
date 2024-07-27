15b67f41b3e2a301826258dbb3b0d1a30fcde571
zahrabateninia <zahrabatenin@gmail.com>
2024-07-27 10:39:27 +0330

    Use a hash table for O(n) time and nested loop for O(n^2)

--- dataStructure/twoNumberSum.py
+++ dataStructure/twoNumberSum.py
@@ -2,3 +2,38 @@
 
 # Usage example: 
 # Input: array = [3,4,-1,11,8] and targetSum=10 -> Output: [-1,11]
+
+# Use a hash table for O(n) time /   O(n) space
+
+def twoNumberSum(array, targetSum):
+    # currentNum + Y = targetSum => Y = targetSum - currentNum -> Add numbers that we have in a dictionary as Y, if Y was there return it
+    nums = {} 
+    for num in array:
+        potentialMatch = targetSum - num 
+        if potentialMatch in nums:
+            return [targetSum - num, num]
+        else:
+            nums[num] = True
+
+    return []
+    
+
+
+
+
+
+
+
+
+
+
+# Worst way to solve it (nested for loop)
+# O(n^2) T , O(1)S:
+def twoNumberSum(array, targetSum):
+    for i in range(len(array) - 1):
+        firstNum = array[i]
+        for j in range(i+ 1, len(array)):
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []
