5c1768f702ca4655322c84cb93ed1a10d1595be6
zahrabateninia <zahrabatenin@gmail.com>
2024-11-26 20:46:35 +0330

    Implement insert method of BST class iteratively

---
+++ binarySearchTrees/BSTConstruction.py
@@ -0,0 +1,42 @@
+# the insert, contains, and remove methods
+# Average: T: O(log(N)) (we eliminate the half), S: O(log(N)) (if implemented recursively), but iteratively: S: O(1)
+# Worst: T: O(N) , S: O(N) (if implemented recursively), but iteratively: S: O(1)
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if currentNode.value < value:
+                if currentNode.value == None:
+                    currentNode.left = BST(value)
+                    break # get out of the loop after inserting
+                else:
+                    currentNode = currentNode.left
+            
+            else:
+                if currentNode.right ==  None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+
+        return self
+
+    def contains(self, value):
+        if self.value == value:
+            return True
+        if (self.value > value and self.left != None):
+            return self.left.contains(value)
+        elif(self.value < value and self.right != None):
+            return self.right.contains(value)
+        else:
+            return False
+
+    def remove(self, value):
+        # Write your code here.
+        # Do not edit the return statement of this method.
+        return self
