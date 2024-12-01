ba286b44ac483ff309e3e7017cc1940c04c0dd0d
zahrabateninia <zahrabatenin@gmail.com>
2024-12-01 23:55:32 +0330

    Wrtie the BST class

---
+++ binarySearchTrees/validateBST.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+# Divide and Conquer approach: T: O(N), S: O()
+def validateBst(tree):
+    # Write your code here.
+    pass
