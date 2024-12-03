efef3dd90ed051e4f8637af221f4d3155b1a2a3f
zahrabateninia <zahrabatenin@gmail.com>
2024-12-03 16:52:30 +0330

    Define a helper function to check if current value is between min and max values

--- binarySearchTrees/validateBST.py
+++ binarySearchTrees/validateBST.py
@@ -6,7 +6,12 @@ class BST:
         self.left = None
         self.right = None
 
-# Divide and Conquer approach: T: O(N), S: O()
+# Divide and Conquer approach: T: O(N), S: O(d) depth of the tree
+
 def validateBst(tree):
-    # Write your code here.
-    pass
+    validateBstHelper(tree, float('-inf'), float('inf'))
+
+def validateBstHelper(tree, minValue, maxValue):
+    # Check if we are on the leaf node 
+    if tree is None:
+        return True # if a tree is none it's a valid BST
