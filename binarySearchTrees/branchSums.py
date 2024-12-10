b377d07a9e63ebc6484bf23ebfbc0f814328a11f
zahrabateninia <zahrabatenin@gmail.com>
2024-12-10 21:02:07 +0330

    Add the prompt of brach sum quesiton

---
+++ binarySearchTrees/branchSums.py
@@ -0,0 +1,7 @@
+# Write a function that takes in a Binary Tree and returns a list of its branch sums ordered from leftmost 
+# branch sum to rightmost branch sum.
+
+# A branch sum is the sum of all values in a Binary Tree branch. A binary Tree branch is a path of nodes in a tree
+# that starts at the root node and ends at tny leaf node.
+
+ 
