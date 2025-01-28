5bdbe34131da790b0e4ec9ab38e8a8eeb10d65c9
zahrabateninia <zahrabatenin@gmail.com>
2025-01-28 16:29:31 +0330

    Update winningTeam and define updateScores function

--- arrays/tournamentWinner.py
+++ arrays/tournamentWinner.py
@@ -12,21 +12,34 @@
 # results[i] denotes the winner of competitions[i], where a 1 in the results means that the home team won and a 0
 #  means that the away team won
 
+# O(n) T n: num of competitions
+# O(k) S
+
 HOME_TEAM_WON = 1 # for better readability
 def tournamentWinner(competitions, results):
-    currentBestTeam = ""
+    currentBestTeam = "" # to keep track of highest score so we don't need another loop for doing that
     scores = {currentBestTeam: 0}
 
     for idx, competition in enumerate(competitions):
         result = results[idx]
-        homeTeam, awayTeam = competition # decomposition
 
+        homeTeam, awayTeam = competition # decomposition
+        # figure out which team is homeTeam and which team is awayTeam
         winningTeam = homeTeam if result == HOME_TEAM_WON else awayTeam
 
         updateScores(winningTeam, 3, scores)
+        if scores[currentBestTeam] < scores[winningTeam]:
+            currentBestTeam = winningTeam
 
     return currentBestTeam
 
 
-def updateScores():
-    pass
+def updateScores(team, points, scores):
+    if team not in scores:
+        scores[team] = 0 # initialize the team that does not exist in the scores with 0 value
+    scores[team] += points
+
+
+# My Notes
+
+# Decomposition in Python in the context of unpacking: This involves breaking down data structures like tuples, lists, or dictionaries into individual variables.
