7e29bdaa600da64e4663199b5ead5f02c444faf3
zahrabateninia <zahrabatenin@gmail.com>
2025-02-11 23:35:17 +0330

    Add the prompt of optimalfreelancing

---
+++ greedyAlgorithms/optimalFreelancing.py
@@ -0,0 +1,15 @@
+# Write a function that returns the max profit that can be obtained in a 7-day period
+# Each job will take one full day to complete
+# Only one job can be worked on at a time
+
+# Sample input:
+# jobs = {
+#     {"deadline":1, "payment":1},
+#     {"deadline":2, "payment":1},
+#     {"deadline":2, "payment":2},
+# } sample output: 3
+
+# O(n Log(n)) T| O(1) S
+def optimalFreelancing(jobs):
+    
+    return -1
