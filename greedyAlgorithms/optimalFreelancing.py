ddcea596d55fbf976a153c66ea582114a68436b5
zahrabateninia <zahrabatenin@gmail.com>
2025-02-12 23:09:28 +0330

    Solve optimalFreelancing using a nested loop and greedy algo

--- greedyAlgorithms/optimalFreelancing.py
+++ greedyAlgorithms/optimalFreelancing.py
@@ -9,9 +9,22 @@
 #     {"deadline":2, "payment":2},
 # } sample output: 3
 
-# O(n Log(n)) T| O(1) S
+# O(n Log(n)) T| O(1) S , n is num of jobs
+
 def optimalFreelancing(jobs):
     LENGTH_OF_WEEK = 7
     profit = 0
-    jobs.sort(key = lambda job: jobs["payment"], reverse=True) 
-    return -1
+    jobs.sort(key = lambda job: job["payment"], reverse=True) 
+
+    # what days are still available -> True 
+    timeline = [False] * LENGTH_OF_WEEK # an array of boolean 
+    for job in jobs: # O(n) for loop
+        # what is the latest possible time in the timeline that we can put it at
+        # max time we consider
+        maxTime = min(job["deadline"], LENGTH_OF_WEEK)
+        for time in reversed(range(maxTime)): # it does not affect the time complexity 
+            if timeline[time] == False:
+                timeline[time] = True
+                profit += jobs["payment"]
+                break # continue the outer for loop
+    return profit
